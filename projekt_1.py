5c1ba1a297b3f0f8adbce5e97ef21086190bdb6f
Łukasz Orszulik <luki93@seznam.cz>
2023-09-05 18:18:22 +0200

    Upravené zobrazení grafu pro 2 verzi textu

--- projekt_1.py
+++ projekt_1.py
@@ -91,9 +91,10 @@ if user in user_list and passw in passw_list:
         print("The sum of all the numbers is", x_sum) if x_sum > 0 else print("There are no numbers") #ternární operátor pro jednoduchost
         print(sep_line)
         #graf na četnost různých delek slov v textu
-        print(f"{'LEN': >3}|{'OCCURENCES': ^15}|{'NR.': <3}")
+        print(f"{'LEN': >3}|{'OCCURENCES': ^18}|{'NR.': <3}")
         print(sep_line)
         len_list = []
+        #zkus zjednodušit
         for word in text_list:
             if word.endswith("."):
                 len_list.append(len(word) - 1)
@@ -108,7 +109,7 @@ if user in user_list and passw in passw_list:
         for lenword in len_list:
             if pocet + 1 in len_list:
                 #print(pocet + 1, "|", "*" * (len_list.count(pocet + 1)), "|", len_list.count(pocet + 1))
-                print(f"{pocet + 1: >3}|{(len_list.count(pocet + 1)) * star: <15}|{len_list.count(pocet + 1):<3}")
+                print(f"{pocet + 1: >3}|{(len_list.count(pocet + 1)) * star: <18}|{len_list.count(pocet + 1):<3}")
             pocet += 1    
                 
 else:
